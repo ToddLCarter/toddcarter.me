bddc4ffa9d3663e6581a41abb33c2ec923de8a30
ToddLCarter <ToddLCarter@users.noreply.github.com>
2022-06-13 12:42:34 +0100

    Animations + mobile responsiveness

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ function getGithubRepo(){
             let card = document.querySelectorAll(`div.card-content`)[i];
             card.querySelector("h2.card-title").innerHTML = repo.name;
             card.querySelector("p.card-text").innerHTML = repo.description;
-            card.querySelector("a.card-link").innerHTML = "View Project";
+            card.querySelector("a.card-link").innerHTML = `<i class="fa-brands fa-github"></i> View Project`;
             card.querySelector("a.card-link").href = repo.html_url;
 
             let cardImage = document.querySelectorAll(`div.card-image`)[i];
@@ -39,3 +39,30 @@ document.querySelector("#mode-toggle").addEventListener("click", function(){
         
     }
 });
+
+// animations
+const updateViewport = () => {
+    // get all elements
+    let elements = document.getElementsByTagName('*');
+    for (let i = 0; i < elements.length; i++) {
+      // get element's top and bottom position
+      let element = elements[i];
+  
+      //skip svg to not break icons
+      if (element.tagName == 'svg') continue;
+  
+      let rect = element.getBoundingClientRect();
+      let top = rect.top;
+      let bottom = rect.bottom;
+  
+      // check if element is in viewport
+      if (top < window.innerHeight && bottom > 0) {
+        element.classList.add('in-viewport');
+      } else {
+        element.classList.remove('in-viewport');
+      }
+    }
+  };
+  
+  updateViewport();
+  document.addEventListener('scroll', updateViewport);
